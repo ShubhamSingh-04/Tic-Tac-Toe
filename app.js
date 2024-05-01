3552e3068ef3b2ebd71cd7016afcd33386afe759
Shubham <shubhamsinghmys@gmail.com>
2024-05-01 20:53:13 +0530

    Basic Operations

---
+++ app.js
@@ -0,0 +1,66 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#resetBtn");
+let turnX = true; 
+let winMessage = document.querySelector("#winMessage");
+
+const winningPatterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                         [0, 3, 6], [0, 4, 8], [1, 4, 7],
+                         [2, 5, 8], [2, 4, 6]];
+
+
+boxes.forEach((box) =>{
+    box.addEventListener("click", ()=>{
+        console.log("Box Clicked");
+        
+        if(turnX){
+            box.innerText = "x";
+            turnX = false;
+        }
+
+        else{
+            box.innerText = "o";
+            turnX = true;
+        }
+
+        box.disabled = true;            // disabling the button
+        checkWinner();
+    })
+});
+
+
+const showWinner = (winner)=>{
+    winMessage.innerText = `The winner is '${winner}'. Congratulations!!`;
+    winMessage.style.visibility = "visible";
+}
+
+
+const checkWinner = () =>{
+    for(pattern of winningPatterns){
+
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+        
+        if(pos1Val === pos2Val && pos2Val === pos3Val){
+            if(pos1Val != ''){
+                console.log("winner:", pos1Val);
+                showWinner(pos1Val);
+            }
+        }
+
+    }
+}
+
+resetBtn.addEventListener("click", () =>{
+    console.log("Reset");
+    for(box of boxes){
+        box.innerText = "";
+        box.disabled = false;
+    }
+    winMessage.style.visibility = "hidden";
+    turnX = true;
+});
+
+resetBtn.addEventListener("mouseover", ()=>{
+    cursor = "pointer";
+})
